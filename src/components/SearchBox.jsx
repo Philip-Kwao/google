e46d038497001c06f637823ef10eff274484b546
Philip Kwao <kwaoizy@gmail.com>
2023-06-30 15:09:20 +0000

    Image Search Page Updated

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -17,6 +17,14 @@ const SearchBox = () => {
     }
     router.push(`/search/web?searchTerm=${search}`)
   }
+
+  const handleImageSearch = () =>{
+    if(!search.trim()){
+      return
+    }
+    router.push(`/search/image?searchTerm=${search}`)
+  }
+
   return (
     <form onSubmit={handleSubmit} action="" className="mt-6 text-center">
         <div className="border flex h-[3rem] w-[600px] items-center rounded-full border-slate-200">
@@ -24,7 +32,7 @@ const SearchBox = () => {
                 <AiOutlineSearch />
             </button>
             <input value={search} type="text" className='flex flex-1 bg-transparent focus:outline-none' onChange={(e)=>setSearch(e.target.value)} />
-            <div className="mx-4 cursor-pointer" onClick={handleSubmit}>
+            <div className="mx-4 cursor-pointer" onClick={handleImageSearch}>
                 <Image src={Camera} height={25} width={25} alt='camera' />
             </div>
         </div>
