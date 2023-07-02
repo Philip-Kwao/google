8f663111baf7dc9cb44f102f755b2eab5fa8329b
Philip Kwao <kwaoizy@gmail.com>
2023-07-02 22:19:58 +0000

    fetching search data with google programmable search

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const page = () => {
+const page = async ({searchParams}) => {
+  const apiKey = process.env.API_KEY
+  const contextKey = process.env.CONTEXT_KEY
+  const res = await fetch(`https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${contextKey}&q=${searchParams.searchTerm}`)
+
+  const data = await res.json()
+  // console.log(data)
+  const results = data.items
+  
   return (
-    <div className='m-10'>Web Search page</div>
+    <div className='m-10'>
+      {results && results.map((result)=>(
+        <div> {result.title} </div>
+      ))}
+    </div>
   )
 }
 
