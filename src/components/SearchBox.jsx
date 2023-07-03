13fc49b674734b816101bce29b2ac0e81eb4a3c9
Philip Kwao <kwaoizy@gmail.com>
2023-07-03 00:36:27 +0000

    Feeling Luck Updated with Random Word API

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -23,25 +23,38 @@ const SearchBox = () => {
       return
     }
     router.push(`/search/image?searchTerm=${search}`)
+    alert("search clicked")
+  }
+  const handleFeelingLucky = async () => {
+    const res = await fetch(`https://random-word-api.vercel.app/api?words=1`)
+    const data = await res.json()
+    router.push(`/search/web?searchTerm=${data}`)
+    // alert(data)
   }
 
   return (
-    <form onSubmit={handleSubmit} action="" className="mt-6 text-center">
-        <div className="border flex h-[3rem] w-[600px] items-center rounded-full border-slate-200">
-            <button type='submit' className="mx-4 text-xl opacity-50">
-                <AiOutlineSearch />
-            </button>
-            <input value={search} type="text" className='flex flex-1 bg-transparent focus:outline-none' onChange={(e)=>setSearch(e.target.value)} />
-            <div className="mx-4 cursor-pointer" onClick={handleImageSearch}>
-                <Image src={Camera} height={25} width={25} alt='camera' />
+    <>
+        <form onSubmit={handleSubmit} action="" className="mt-6 text-center">
+            <div className="border flex h-[3rem] w-[600px] items-center rounded-full border-slate-200">
+                <button type='submit' className="mx-4 text-xl opacity-50">
+                    <AiOutlineSearch />
+                </button>
+                <input value={search} type="text" className='flex flex-1 bg-transparent focus:outline-none' onChange={(e)=>setSearch(e.target.value)} />
+                <div className="mx-4 cursor-pointer" onClick={handleImageSearch}>
+                    <Image src={Camera} height={25} width={25} alt='camera' />
+                </div>
             </div>
+            {/* 2 buttons */}
+        </form>
+        <div className="m-5 flex">
+          <div onClick={handleSubmit} className="">
+            <Button btnTitle={'Google Search'} />
+          </div>
+          <div onClick={handleFeelingLucky} className="">
+            <Button btnTitle={'Feeling Lucky'} />
+          </div>
         </div>
-        {/* 2 buttons */}
-      <div className="m-5">
-        <Button onClick={handleSubmit} btnTitle={'Google Search'} />
-        <Button btnTitle={'Feeling Lucky'} />
-      </div>
-    </form>
+      </>
   )
 }
 
